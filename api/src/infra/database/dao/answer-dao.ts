e06de368bc0e720bfb4b8fdc8ffb0cdd7dee2181
gersonvideira <gersonvideira@gmail.com>
2024-02-20 00:34:50 +0100

    Create answer-dao and question-dao

--- api/src/infra/database/dao/answer-dao.ts
+++ api/src/infra/database/dao/answer-dao.ts
@@ -1,13 +1,23 @@
 import DAO from "@domain/dao/dao";
-import AnswerEntity from "@domain/entity/answer-entity";
+import {Answer} from "@domain/model";
+import {KnexTypeAdapter, DatabaseTableNames } from "../knex-adapter";
 
 
-export default class AnswerDAO implements DAO<AnswerEntity>{
-    create(data: AnswerEntity): Promise<AnswerEntity> {
-        throw new Error('Method not implemented.');
+export default class AnswerDAO implements DAO<Answer.Model>{
+    private readonly tableName: string = DatabaseTableNames.ANSWERS
+    constructor(private readonly connection: KnexTypeAdapter){}
+
+    async create(data: Answer.Model): Promise<Answer.Model> {
+        const [answer] = await this.connection<Answer.Model>(this.tableName)
+        .insert(data).returning('*')
+        return answer
     }
-    findById(id: string): Promise<AnswerEntity> {
-        throw new Error("Method not implemented.");
+    async findById(answerId: string): Promise<Answer.Model | null> {
+        const data = await this.connection<Answer.Model>(this.tableName)
+        .where({answerId}).first()
+
+        if(!data) return null
+        return data
     }
 
 }
--- api/src/infra/database/dao/question-dao.ts
+++ api/src/infra/database/dao/question-dao.ts
@@ -1,13 +1,24 @@
 import DAO from "@domain/dao/dao";
-import UserEntity from "@domain/entity/user-entity";
+import { Question } from "@domain/model";
+import { KnexTypeAdapter, DatabaseTableNames } from "../knex-adapter";
 
 
-export default class UserDAO implements DAO<UserEntity>{
-    create(data: UserEntity): Promise<UserEntity> {
-        throw new Error('Method not implemented.');
+export default class UserDAO implements DAO<Question.Model>{
+    private readonly tableName: string = DatabaseTableNames.QUESTIONS
+    constructor(private readonly connection: KnexTypeAdapter){}
+
+    async create(data: Question.Model): Promise<Question.Model> {
+        const [question] = await this.connection<Question.Model>(this.tableName)
+        .insert(data).returning('*')
+
+        return question
     }
-    findById(id: string): Promise<UserEntity> {
-        throw new Error("Method not implemented.");
+    async findById(questionId: string): Promise<Question.Model | null> {
+        const data = await this.connection<Question.Model>(this.tableName)
+        .where({questionId}).first()
+
+        if(!data) return null
+        return data
     }
 
 }
